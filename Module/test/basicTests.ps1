8be6a1c91f4ad9116588719f9741249f2dcb6fa7
Richard Carpenter <artfulbodger80@gmail.com>
2019-03-19 21:36:13 +0000

    Update module to use Public/Private functions
    Signed-off-by: Richard Carpenter <artfulbodger80@gmail.com>

--- Module/test/basicTests.ps1
+++ Module/test/basicTests.ps1
@@ -1,13 +1,12 @@
-$projectRoot = Resolve-Path "$PSScriptRoot\.."
-$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
+$moduleRoot = Resolve-Path "$PSScriptRoot\.."
 $moduleName = Split-Path $moduleRoot -Leaf
 
 Describe "General project validation: $moduleName" {
 
-    $scripts = Get-ChildItem $projectRoot -Include *.ps1,*.psm1,*.psd1 -Recurse
+    $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
 
     # TestCases are splatted to the script so we need hashtables
-    $testCase = $scripts | Foreach-Object{@{file=$_}}         
+    $testCase = $scripts | Foreach-Object {@{file = $_}}         
     It "Script <file> should be valid powershell" -TestCases $testCase {
         param($file)
 
@@ -17,9 +16,5 @@ Describe "General project validation: $moduleName" {
         $errors = $null
         $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
         $errors.Count | Should Be 0
-    }
-
-    It "Module '$moduleName' can import cleanly" {
-        {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
     }
 }
