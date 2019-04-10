4bd2e638949a43935d9bee955e6e23017b467aaf
Richard Carpenter <richard.carpenter@global.com>
2019-04-10 09:06:32 +0100

    Add new Function Template and Tests

---
+++ Function/functionTemplate.ps1
@@ -0,0 +1,62 @@
+﻿<%
+"function $PLASTER_PARAM_FunctionName"
+%>
+{
+<%
+    If ($PLASTER_PARAM_Help -eq 'Yes')
+    {
+        @"
+  <#
+    .SYNOPSIS
+      Describe purpose of $PLASTER_PARAM_FunctionName in 1-2 sentences.
+
+      .DESCRIPTION
+      Add a more complete description of what the function does.
+
+      .PARAMETER Param1
+      Describe parameter -Param1.
+
+      .EXAMPLE
+      $PLASTER_PARAM_FunctionName -Param1 Value
+      Describe what this call does
+
+      .NOTES
+      Place additional notes here.
+
+      .LINK
+      URLs to related sites
+      The first link is opened by Get-Help -Online $PLASTER_PARAM_FunctionName
+  #>
+"@
+    }
+%>
+<%
+    if ($PLASTER_PARAM_CmdletBinding -eq 'Simple')
+    {
+@'
+
+      [CmdletBinding()]
+'@
+    }
+    else 
+    {
+@'
+      [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+'@
+    }
+%>
+    Param
+    (
+        [Parameter(Mandatory,HelpMessage='Add help message for user')][string]$Param1
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
